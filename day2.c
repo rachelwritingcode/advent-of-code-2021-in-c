151be79a520f66e54dbc80ac7f6c0245dfaac87e
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2023-11-12 13:46:00 -0500

    Add day 2 skeleton code - wip

---
+++ day2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 2000
+
+int main() {
+    // Open input file
+    FILE *file = fopen("day2_test.txt", "r");
+    int depths[MAX];
+
+    if (file != NULL) {
+        
+    } 
+    return 0;
+}
+
